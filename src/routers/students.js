3ca160ab6779a3c8ea3cb3da03d04399e4924163
anastasia1sl <slobodyanik.a97@gmail.com>
2024-08-09 15:54:50 +0300

    validation joi

--- src/routers/students.js
+++ src/routers/students.js
@@ -3,6 +3,12 @@ import { createStudentController } from '../controllers/students.js';
 import { deleteStudentController } from '../controllers/students.js';
 import { upsertStudentController } from '../controllers/students.js';
 import { patchStudentController } from '../controllers/students.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from '../validation/students.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 import {
   getStudentsController,
@@ -15,23 +21,35 @@ const studentsRouter = Router();
 studentsRouter.get('/students', ctrlWrapper(getStudentsController));
 studentsRouter.get(
   '/students/:studentId',
+  isValidId,
   ctrlWrapper(getStudentByIdController),
 );
 
 studentsRouter.post('/students', ctrlWrapper(createStudentController));
 studentsRouter.delete(
   '/students/:studentId',
+  isValidId,
   ctrlWrapper(deleteStudentController),
 );
 
 studentsRouter.put(
   '/students/:studentId',
+  isValidId,
+  validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
 
 studentsRouter.patch(
   '/students/:studentId',
+  isValidId,
+  validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
+);
+
+studentsRouter.post(
+  '/',
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
 );
 
 export default studentsRouter;
